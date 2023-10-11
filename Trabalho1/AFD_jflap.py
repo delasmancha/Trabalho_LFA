c08d2c94f9f8b64d23234525976898e75d7c8371
delasmancha <84483939+delasmancha@users.noreply.github.com>
2023-10-11 16:12:27 -0300

    Lendo e Escrevendo pelo JFLAP

--- Trabalho1/AFD_jflap.py
+++ Trabalho1/AFD_jflap.py
@@ -1,10 +1,11 @@
 #from AFD import *
 import xml.etree.ElementTree as ET
+from AFD import AFD
 
 def lerAFD():
-    local = input("Digite o local do arquivo a ser lido: ")
+    #local = input("Digite o local do arquivo a ser lido: ")
     # Ler o conteúdo do arquivo
-    with open('JFLAP/teste1.jff', 'r') as p:
+    with open('Trabalho1/teste1.jff', 'r') as p:
         xml_content = p.read()
 
     # Analisa o conteúdo XML
@@ -38,22 +39,72 @@ def lerAFD():
         for aux1 in transicao.findall('./to'):
             s = aux1.text
             destino.append(int(s)+1)
-        
+    alf = ""
+    estado = []
     for i in range(len(car)):
         aux2[0] = origem[i]
+        if(aux2[0] not in estado):
+            estado.append(aux2[0])
         aux2[1] = car[i]
+        if(car[i] not in alf):
+            alf = alf + car[i]
         aux2[2] = destino[i]
         aux3 = []
         aux3=aux3 + aux2
         lista_es.append(aux3)
-
 
     # Imprime os resultados
     afd.append(id_inicial)
     afd.append(ids_finais)
     afd.append(lista_es)
 
-    return afd
+    afd_res = AFD(alf)
+
+    for i in range(len(estado)):
+        afd_res.criaEstado(i+1)
+
+    afd_res.mudaEstadoInicial(afd[0]) #Pegando o Estado Inicial
+
+    for i in range(len(afd[1])):#Pegando os Estados Finais
+        afd_res.mudaEstadoFinal(afd[1][i], True)
+
+    for i in range(len(afd[2])):#Pegando as Transições
+        afd_res.criaTransicao(afd[2][i][0], afd[2][i][2], afd[2][i][1])
+
+
+    print(afd_res)
+
+    return afd_res
 
 afd = lerAFD()
+
+def escreveAFD(afd):
+    #local = input("Digite o local do arquivo a ser lido: ")
+    # Ler o conteúdo do arquivo
+    a = open("Trabalho1/a1.jff", "w")
+    a.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?><!--Created with JFLAP 7.1.--><structure>&#13;')
+    a.write("\n<type>fa</type>&#13;")
+    a.write("\n<automaton>&#13;")
+    for i in afd.estados:
+        aux = i
+        aux = i -1
+        q = 'q'
+        q = q + str(aux)
+        a.write('\n<state id="{}" name="{}">&#13;'.format(aux, aux))
+        if(i == afd.inicial):
+            a.write('\n<initial/>&#13;')
+        if(i in afd.finais):
+            a.write('\n<final/>&#13;')
+        a.write("\n</state>&#13;")
+
+    for i in afd.transicoes.items():
+        a.write('\n<transition>&#13;')
+        a.write('\n<from>{}</from>&#13;'.format(i[0][0]-1))
+        a.write('\n<to>{}</to>&#13;'.format(i[1]-1))
+        a.write('\n<read>{}</read>&#13;'.format(i[0][1]))
+        a.write('\n</transition>&#13;')
+    a.write('\n</automaton>&#13;')
+    a.write('\n</structure>')
+    a.close()
     
+escreveAFD(afd)
